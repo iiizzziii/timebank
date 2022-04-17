b66a0e5c04701126616e9cfade9efc3d5c368c6b
Jakub Mokoš <mokos.jakub@gmail.com>
2022-04-17 10:51:18 +0200

    localhost

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ import sqlalchemy
 
 app = Flask(__name__)
 db = SQLAlchemy()
-# meno = root / heslo = timebank (tiez v aws)
-# timebank.c6fpgwhpaeto.eu-central-1.rds.amazonaws.com
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:timebank@127.0.0.1:3306/timebank")
-connection = engine.connect() # optional???
-SQLALCHEMY_DATABASE_URI = "mariadb+mariadbconnector://root:timebank@127.0.0.1:3306/timebank"
+
+DB_HOST = "127.0.0.1"
+# DB_HOST = "timebank.c6fpgwhpaeto.eu-central-1.rds.amazonaws.com" # AWS
+DB_PORT = "3306"
+DB_NAME = "timebank"
+DB_USERNAME = "root"
+DB_PASSWORD = "timebank"  
+
+engine = sqlalchemy.create_engine(f"mariadb+mariadbconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+connection = engine.connect()
+SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 Base = declarative_base()
@@ -22,40 +28,38 @@ class User(Base):
     phone = db.Column(db.String(13), nullable=False, unique=True)
     password = db.Column(db.String(30), nullable=False)
 
-class Service(Base):
-    __tablename__ = 'services'
+class Job(Base):
+    __tablename__ = 'jobs'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     description = db.Column(db.String(300), nullable=False, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
-    user = db.relationship('User', order_by='User.id', backref='service')
+    user = db.relationship('User', order_by='User.id', backref='job')
 
 class Deal(Base):
     __tablename__ = 'deals'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey("jobs.id"), nullable=False)
     consumer_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    # service_id.user_id != consumer_id
+    # consumer_id != job_id.user_id
     date = db.Column(db.Date)
     hours = db.Column(db.Integer)
     done = db.Column(db.Boolean, default=False)
 
-    service = db.relationship('Service', order_by='Service.id', backref='deals')
+    job = db.relationship('Job', order_by='Job.id', backref='deals')
     consumer = db.relationship('User', order_by='User.id', backref='deals')
 
-# "HANDLE_DB"
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 # Base.metadata.drop_all(engine)
 
 # Session = sqlalchemy.orm.sessionmaker()
 # Session.configure(bind=engine)
 # Session = Session()
 
-# PRIDAVANIE DAT DO DB ***AK NECHCETE PRIDAVAT PO RIADKOCH ZMENTE PREMENNU "add2db"***
 # add2db = User(name='lacike',phone='+421999999999',password='heslo1')
 # add2db = User(name='ferko',phone='+421777777777',password='heslo2')
-# add2db = Service(description='pepe',user_id=1)
-# add2db = Deal(service_id=1,consumer_id=2,date=datetime.now(),hours=1)
+# add2db = Job(description='pepe',user_id=1)
+# add2db = Deal(job_id=1,consumer_id=2,date=datetime.now(),hours=1)
 
 # Session.add(add2db)
 # Session.commit()
